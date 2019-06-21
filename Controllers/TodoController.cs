e7587e98f5c99c53337a40cb52aa1e6a64a51b94
Taj <tajbi21@gmail.com>
2019-06-21 10:40:46 +0100

    created our first view

--- Controllers/TodoController.cs
+++ Controllers/TodoController.cs
@@ -4,12 +4,13 @@ using System.Linq;
 using TodoApi.Models; 
 using System.Threading;
 using System.Threading.Tasks; 
-using Microsoft.AspNetCore.Mvc; 
+using Microsoft.AspNetCore.Mvc;
+
 
 namespace TodoApi.Controllers {     
     [Route("api/[controller]")]     
     [ApiController]     
-    public class TodoController : ControllerBase     
+    public class TodoController : Controller     
     {        
         private readonly TodoContext _context;          
         public TodoController(TodoContext context)         
@@ -20,51 +21,56 @@ namespace TodoApi.Controllers {
              _context.SaveChanges();             
             }         
         }
-        
-        [HttpGet] 
-        public ActionResult<List<TodoItem>> GetAll() 
-        {     
-            return _context.TodoItems.ToList(); 
-        } 
-        
-        [HttpGet("{id}", Name = "GetTodo")] 
-        public ActionResult<TodoItem> GetById(long id) 
-        {    
-            var item = _context.TodoItems.Find(id);     
-            if (item == null)    
-            {         
-                return NotFound();     
-            }     
-            return item; 
-        }
 
-        [HttpPost]
-        public async Task<ActionResult<TodoItem>> PostTodoItem(TodoItem item)
+        public IActionResult Index()
         {
-            _context.TodoItems.Add(item);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction(nameof(GetById), new { id = item.Id }, item);
+            return View();
         }
+        
+        // [HttpGet] 
+        // public ActionResult<List<TodoItem>> GetAll() 
+        // {     
+        //     return _context.TodoItems.ToList(); 
+        // } 
+        
+        // [HttpGet("{id}", Name = "GetTodo")] 
+        // public ActionResult<TodoItem> GetById(long id) 
+        // {    
+        //     var item = _context.TodoItems.Find(id);     
+        //     if (item == null)    
+        //     {         
+        //         return NotFound();     
+        //     }     
+        //     return item; 
+        // }
 
-        [HttpPut("{id}", Name = "UpdateTodo")]
+        // [HttpPost]
+        // public async Task<ActionResult<TodoItem>> PostTodoItem(TodoItem item)
+        // {
+        //     _context.TodoItems.Add(item);
+        //     await _context.SaveChangesAsync();
 
-        public async Task<ActionResult<TodoItem>> UpdateTodo(long id, string name) 
-        {
-            var item = _context.TodoItems.Find(id);
-            item.Name = name;
+        //     return CreatedAtAction(nameof(GetById), new { id = item.Id }, item);
+        // }
 
-            await _context.SaveChangesAsync();
+        // [HttpPut("{id}", Name = "UpdateTodo")]
 
-            return item;
-        }
+        // public async Task<ActionResult<TodoItem>> UpdateTodo(long id, string name) 
+        // {
+        //     var item = _context.TodoItems.Find(id);
+        //     item.Name = name;
 
-        [HttpDelete("{id}", Name = "DeleteTodo")]
-        public async Task<ActionResult<string>> DeleteTodoItem(long id) {
-            var item = _context.TodoItems.Find(id); 
-            _context.TodoItems.Remove(item);
-            await _context.SaveChangesAsync();
-            return "Successfully deleted";
-        }
+        //     await _context.SaveChangesAsync();
+
+        //     return item;
+        // }
+
+        // [HttpDelete("{id}", Name = "DeleteTodo")]
+        // public async Task<ActionResult<string>> DeleteTodoItem(long id) {
+        //     var item = _context.TodoItems.Find(id); 
+        //     _context.TodoItems.Remove(item);
+        //     await _context.SaveChangesAsync();
+        //     return "Successfully deleted";
+        // }
     } 
 }
--- Startup.cs
+++ Startup.cs
@@ -14,34 +14,44 @@ using Microsoft.EntityFrameworkCore;
 using TodoApi.Models;
 namespace TodoApi
 {
-public class Startup
-{
-public Startup(IConfiguration configuration)
-{
-Configuration = configuration;
-}
-public IConfiguration Configuration { get; }
-// This method gets called by the runtime. Use this method to add services to the container.
-public void ConfigureServices(IServiceCollection services)
-{
-// Add framework services.
-services.AddDbContext<TodoContext>(options =>
-options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection")));
-services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-}
-// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-{
-if (env.IsDevelopment())
-{
-app.UseDeveloperExceptionPage();
-}
-else
-{
-app.UseHsts();
-}
-app.UseHttpsRedirection();
-app.UseMvc();
-}
-}
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add framework services.
+            services.AddDbContext<TodoContext>(options =>
+            options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+            // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+            app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+            app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+
+            app.UseMvc(routes => {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Todo}/{action=Index}/{id?}"
+                );
+            } );
+
+        }
+
+    }
 }
